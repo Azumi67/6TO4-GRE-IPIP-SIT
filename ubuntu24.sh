defea987f8c2089cb8a5684fa2cd4a00f466049b
Azumi <azumitan67@gmail.com>
2024-05-28 18:44:27 +0330

    Update ubuntu24.sh

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+apt update -y
+apt install wget -y
 wget -O /etc/logo.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo.sh
 chmod +x /etc/logo.sh
 wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/test/ubuntu24.py
