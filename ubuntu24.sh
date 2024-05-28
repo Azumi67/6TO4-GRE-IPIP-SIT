56dd65d2f5fbaaab3295d164b4e501324a406f70
Azumi <azumitan67@gmail.com>
2024-05-28 00:24:49 -0700

    Create ubuntu24.sh

---
+++ ubuntu24.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -O /etc/logo.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo.sh
+chmod +x /etc/logo.sh
+wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/test/ubuntu24.py
+python3 ubuntu24.py
