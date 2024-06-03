eb66129e5c164540c4dda0d1ce21a24218a12dd0
Azumi <azumitan67@gmail.com>
2024-06-02 23:12:40 -0700

    Create ubuntu24-2.sh

---
+++ ubuntu24-2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update -y
+apt install wget -y
+wget -O /etc/logo.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
+chmod +x /etc/logo.sh
+if [ -f "ubuntu24p2.py" ]; then
+    rm ubuntu24p2.py
+fi
+wget https://github.com/Azumi67/6TO4-GRE-IPIP-SIT/releases/download/ubuntu24/ubuntu24p2.py
+python3 ubuntu24p2.py
