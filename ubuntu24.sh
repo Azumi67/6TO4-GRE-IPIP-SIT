2d1ea6128852e63d41154a2f8fac113f58e87616
Azumi <azumitan67@gmail.com>
2024-06-21 00:02:25 -0700

    Update ubuntu24.sh

--- ubuntu24.sh
+++ ubuntu24.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update -y
 apt install wget -y
-wget -O /etc/logo.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
+wget -O /etc/logo2.sh https://github.com/Azumi67/UDP2RAW_FEC/raw/main/logo2.sh
 chmod +x /etc/logo2.sh
 if [ -f "ubuntu24.py" ]; then
     rm ubuntu24.py
