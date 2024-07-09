80965262cc044157d0e19ed7d140712de586088b
Azumi <azumitan67@gmail.com>
2024-07-08 22:54:58 -0700

    Create managed3.sh

---
+++ managed3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+##extemely managed server
+
+sudo apt update
+
+package_name=$(apt search python3-netifaces | grep -o '^python3-netifaces\S*')
+
+if [[ -n "$package_name" ]]; then
+    sudo apt install "$package_name"
+else
+    echo "Package python3-netifaces not found in the repositories."
+fi
+
+package_name=$(apt search python3-colorama | grep -o '^python3-colorama\S*')
+
+if [[ -n "$package_name" ]]; then
+    sudo apt install "$package_name"
+else
+    echo "Package python3-colorama not found in the repositories."
+fi
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/Azumi67/6TO4-GRE-IPIP-SIT/main/ubuntu24.sh)"
